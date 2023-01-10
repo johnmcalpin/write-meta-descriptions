180482f1be4915898f0e1d84a0e98d61bbd3f6e0
John McAlpin <90294960+johnmcalpin@users.noreply.github.com>
2023-01-10 12:58:12 -0700

    Update write-meta-descriptions.py

--- write-meta-descriptions.py
+++ write-meta-descriptions.py
@@ -1,4 +1,3 @@
-import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -12,17 +11,20 @@ results = []
 for url in urls:
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-
-    # 3) writes a meta description for each URL with a maximum character count of 155 characters
-    description = soup.find('meta', {'name': 'description'}).get('content')
-    if len(description) > 155:
-        description = description[:152] + '...'
-
-    results.append({
-        'url': url,
-        'description': description
-    })
-
+    description_tag = soup.find('meta', {'name': 'description'})
+    if description_tag:
+        description = description_tag.get('content')
+        # 3) writes a meta description for each URL with a maximum character count of 155 characters
+        if len(description) >= 135 and len(description) <= 160:
+            results.append({
+                'url': url,
+                'description': description
+            })
+    else:
+        results.append({
+                'url': url,
+                'description': ' '
+        })
 # 4) exports the results to a csv file
 with open('results.csv', 'w', newline='') as f:
     writer = csv.DictWriter(f, fieldnames=['url', 'description'])
