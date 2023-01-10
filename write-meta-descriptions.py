27b48c13d02eb02b4489935b039cd59c8e699de4
John McAlpin <90294960+johnmcalpin@users.noreply.github.com>
2023-01-10 13:22:41 -0700

    Update write-meta-descriptions.py

--- write-meta-descriptions.py
+++ write-meta-descriptions.py
@@ -1,32 +1,34 @@
-import requests
-from bs4 import BeautifulSoup
+!pip install sumy
+from sumy.parsers.html import HtmlParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+from sumy.summarizers.lsa import LsaSummarizer
+import csv
 
-# 1) imports a list of URLs from a txt file
-with open('/content/drive/MyDrive/urls.txt') as f:
+#1) imports a list of URLs from a txt file
+with open('urls.txt') as f:
     urls = [line.strip() for line in f]
 
 results = []
 
 # 2) analyzes the content on each URL
 for url in urls:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    description_tag = soup.find('meta', {'name': 'description'})
-    if description_tag:
-        description = description_tag.get('content')
-        # 3) writes a meta description for each URL with a maximum character count of 155 characters
-        if len(description) >= 135 and len(description) <= 160:
-            results.append({
-                'url': url,
-                'description': description
-            })
-    else:
-        results.append({
-                'url': url,
-                'description': ' '
-        })
+    parser = HtmlParser.from_url(url, Tokenizer("english"))
+    stemmer = Stemmer("english")
+    summarizer = LsaSummarizer(stemmer)
+    summarizer.stop_words = get_stop_words("english")
+    description = summarizer(parser.document, 3)
+    description = " ".join([sentence._text for sentence in description])
+    if len(description) > 155:
+        description = description[:152] + '...'
+    results.append({
+        'url': url,
+        'description': description
+    })
+
 # 4) exports the results to a csv file
 with open('results.csv', 'w', newline='') as f:
-    writer = csv.DictWriter(f, fieldnames=['url', 'description'])
+    writer = csv.DictWriter(f, fieldnames=['url','description'])
     writer.writeheader()
     writer.writerows(results)
